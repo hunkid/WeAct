b1638303d1b5e3cdaa515d871565b20848c4ddc3
hunkid <273946286@qq.com>
2017-06-29 17:53:55 +0800

    初次撰写

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <el-col id="sidebar">
+      sss
+      <el-menu default-active="1" class="el-menu-vertical-demo" theme="light" router="router">
+        <el-menu-item index="1">导航一</el-menu-item>
+        <el-menu-item index="2">导航二</el-menu-item>
+        <el-menu-item index="3">导航三</el-menu-item>
+      </el-menu>
+    </el-col>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#sidebar {
+  position: fixed;
+  width: 200px;
+  height: 100%;
+  background-color: #eef1f6;
+}
+</style>
